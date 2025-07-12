9968dac7521738a731b843bfcc7dae5e16e7d0b6
Caspy <acadpoly@gmail.com>
2025-07-12 21:39:27 +0800

    Update init.pp

--- init.pp
+++ init.pp
@@ -8,3 +8,36 @@ service { 'ufw':
   enable => true,
 }
 
+# Ensure OpenSSH server is installed
+package { 'openssh-server':
+  ensure => installed,
+}
+
+# SSH configuration file
+file { '/etc/ssh/sshd_config':
+  ensure  => file,
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  content => template('/tmp/manifests/sshd_config.erb'),
+  notify  => Service['ssh'],
+}
+
+# SSH banner
+file { '/etc/issue.net':
+  ensure  => file,
+  content => "️Authorized access only. All activity will be monitored.\n",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  notify  => Service['ssh'],
+}
+
+# Ensure SSH service is running and restarts on config change
+service { 'ssh':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  hasstatus  => true,
+}
+
